f3f0ad8e84ad3abe724aa4ee36061c50c90ffb15
Kayla Kingston <kkingston09@gmail.com>
2020-06-10 19:23:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,14 +4,10 @@ class Backer
   
   def initialize(name)
     @name = name
-    @project = project
-  end 
-  
-  def backed_projects
     @backed_projects = []
   end 
   
   def back_project(project)
-    @backed_projects << @project
+    @backed_projects << project
   end
 end 
